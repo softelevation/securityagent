8b7ed98172b3c5bf93fd12a68f4e6f4a83b5814a
softelevation <vsandhu@softelevation.com>
2020-10-16 15:25:16 +0530

    dddddddd

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -19,7 +19,7 @@
   <script src="{{asset('assets/js/jquery.min.js')}}"></script>
   <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('assets/plugins/datetimepicker/jquery.datetimepicker.full.min.js')}}"></script>
+  <script src="{{asset('assets/plugins/datetimepicker/jquery.datetimepicker.'.__('frontend.language').'.min.js')}}"></script>
   <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
   <style type="text/css"> 
