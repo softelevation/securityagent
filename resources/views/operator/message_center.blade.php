e3d5280d81a226d66f0bc71c1ea6e25f211f45e8
vijay sandhu <vijay.pro2020@gmail.com>
2021-04-20 15:58:24 +0530

    fix code

--- resources/views/operator/message_center.blade.php
+++ resources/views/operator/message_center.blade.php
@@ -46,7 +46,6 @@ div.ex1 {background-color: lightblue; height: 325px; overflow: scroll; padding: 
 						
                           </div>
 						  <div class="view_agent_details mt-4">
-								{{Form::model($profile,['url'=>url('operator/message-center'),'id'=>'message_center'])}}
 								  <div class="row">
 									<div class="col-md-8 form-group">
 									  {{Form::textarea('send_message',null,['data-id'=>$user_id,'data-cus_id'=>$cus_id,'class'=>'form-control message-center','placeholder'=>__('frontend.text_152')])}}
@@ -55,7 +54,6 @@ div.ex1 {background-color: lightblue; height: 325px; overflow: scroll; padding: 
 										<input type="submit" class="yellow_btn" value="{{__('frontend.text_73')}}"/>
 									</div>
 								  </div>
-								  {{Form::close()}}
 							</div>
 						  
                       </div>
@@ -83,8 +81,6 @@ div.ex1 {background-color: lightblue; height: 325px; overflow: scroll; padding: 
 	// http://localhost:7000/socket.io/socket.io.js
 	$('.message-center-child').scrollTop($('.message-center-child')[0].scrollHeight);
 	 var socket = io.connect('http://51.68.139.99:3000');
-	// console.log(socket);
-	
 	
 	socket.on('message_center_'+mission_id,function(msg){
 		if(msg){
@@ -92,38 +88,13 @@ div.ex1 {background-color: lightblue; height: 325px; overflow: scroll; padding: 
 		}
 	});
 			
-			
-	$('#message_center').submit(function(event){
-			socket.emit('op_message_center',{
-				mission_id:mission_id,
-				user_id:cus_id,
-				message:$('textarea[name="send_message"]').val()
-			});
-			
-			// var message = $('textarea[name="send_message"]').val();
-			// var user_id = $('textarea[name="send_message"]').data('id');
-			// var cus_id = $('textarea[name="send_message"]').data('cus_id');
-			// var mission_id = $('textarea[name="send_message"]').data('mission_id');
-			// var formData = new FormData(this);
-			// formData.append('user_id', user_id);
-			// formData.append('cus_id', cus_id);
-			// formData.append('mission_id', mission_id);
-			// $.ajax({
-				   // type:this.method,
-				   // url: this.action, 
-				   // contentType: false, 
-				   // processData:false,
-				   // data: formData,
-				   // success:function(response)
-				   // {
-					   
-					   // if(response.status == 1)
-						   // $('textarea[name="send_message"]').val('');
-						   // $(".message_last").after('<p class="'+response.message_type+'"><b>'+response.message+' :</b>'+message+'</p>');
-						   // location.reload();
-				   // }
-			// });
-			event.preventDefault();
+	$('input[type="submit"]').click(function(event){
+		socket.emit('op_message_center',{
+			mission_id:mission_id,
+			user_id:cus_id,
+			message:$('textarea[name="send_message"]').val()
 		});
+	});
+			
 </script>
 @endsection
